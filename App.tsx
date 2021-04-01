128678fb7278af2a3877f476652683d2a9582545
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-01 17:10:37 -0300

    config: adding global styles

--- App.tsx
+++ App.tsx
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+import { ThemeProvider } from "styled-components";
+
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Inter_500Medium, Inter_700Bold } from "@expo-google-fonts/inter";
+import theme from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+  if (!fontsLoaded) return null;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/styles/theme.ts
@@ -0,0 +1,40 @@
+export default {
+  colors: {
+    background: "#FFFFFF",
+    shape: "#E5E5E5",
+    blue600: "#2E3E5C",
+    blue700: "#3E5481",
+    green: "#1FCC79",
+    gray: "#9FA5C0",
+    skeleton: "#969590",
+    blue: "#1851ad",
+  },
+
+  textVariants: {
+    title: {
+      fontSize: "17px",
+      fontFamily: "Roboto_700Bold",
+      lineHeight: "27px",
+    },
+    subtitle: {
+      fontSize: "17px",
+      fontFamily: "Roboto_400Regular",
+      lineHeight: "27px",
+    },
+    label: {
+      fontSize: "17px",
+      fontFamily: "Inter_700Bold ",
+      lineHeight: "27px",
+    },
+    content: {
+      fontSize: "15px",
+      fontFamily: "Inter_500Medium",
+      lineHeight: "25px",
+    },
+    button: {
+      fontSize: "15px",
+      fontFamily: "Inter_500Medium",
+      lineHeight: "25px",
+    },
+  },
+} as const;
